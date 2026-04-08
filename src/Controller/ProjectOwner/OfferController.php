1e0b2748a18235afdc4b5eb6639b5c904765697b
anasfdl1 <mohamedanas.chagour@esprit.tn>
2026-04-08 03:39:14 +0100

    regler l ajout modification cote project owner

--- src/Controller/ProjectOwner/OfferController.php
+++ src/Controller/ProjectOwner/OfferController.php
@@ -33,28 +33,27 @@ class OfferController extends AbstractController
         ]);
     }
 
-    #[Route('/project-owner/offers/new', name: 'app_project_owner_offer_new')]
-    public function new(
-        Request $request,
-        EntityManagerInterface $entityManager,
-        SluggerInterface $slugger
-    ): Response {
-        $user = $this->getUser();
+   #[Route('/project-owner/offers/new', name: 'app_project_owner_offer_new')]
+public function new(
+    Request $request,
+    EntityManagerInterface $entityManager,
+    SluggerInterface $slugger
+): Response {
+    $offer = new Offers();
+    $offer->setStatus('DRAFT');
+    $offer->setCreatedAt(new \DateTime());
 
-        if (!$user) {
-            throw $this->createAccessDeniedException('Utilisateur non authentifié.');
-        }
+    /** @var \App\Entity\Users|null $user */
+    $user = $this->getUser();
+    if ($user) {
+        $offer->setCreatedBy($user);
+    }
 
-        $offer = new Offers();
+    $form = $this->createForm(ProjectOwnerOfferType::class, $offer);
+    $form->handleRequest($request);
 
-        $form = $this->createForm(ProjectOwnerOfferType::class, $offer);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $offer->setStatus('DRAFT');
-            $offer->setCreatedAt(new \DateTime());
-            $offer->setCreatedBy($user);
-
+    if ($form->isSubmitted()) {
+        if ($form->isValid()) {
             $imageFile = $form->get('imageFile')->getData();
 
             if ($imageFile) {
@@ -67,25 +66,35 @@ class OfferController extends AbstractController
                         $this->getParameter('offers_images_directory'),
                         $newFilename
                     );
+
                     $offer->setImageUrl($newFilename);
                 } catch (FileException $e) {
                     $this->addFlash('error', 'Erreur lors de l’upload de l’image.');
+                    return $this->render('project_owner/offer/new.html.twig', [
+                        'form' => $form->createView(),
+                    ]);
                 }
             }
 
-            $entityManager->persist($offer);
-            $entityManager->flush();
+            try {
+                $entityManager->persist($offer);
+                $entityManager->flush();
 
-            $this->addFlash('success', 'Offre créée avec succès.');
+                $this->addFlash('success', 'Offre ajoutée avec succès.');
 
-            return $this->redirectToRoute('app_project_owner_offer_list');
+                return $this->redirectToRoute('app_project_owner_offer_list');
+            } catch (\Exception $e) {
+                $this->addFlash('error', 'L’offre n’a pas pu être enregistrée.');
+            }
+        } else {
+            $this->addFlash('error', 'Veuillez corriger les erreurs du formulaire.');
         }
-
-        return $this->render('project_owner/offer/new.html.twig', [
-            'form' => $form->createView(),
-        ]);
     }
 
+    return $this->render('project_owner/offer/new.html.twig', [
+        'form' => $form->createView(),
+    ]);
+}
     #[Route('/project-owner/offers/{id}/edit', name: 'app_project_owner_offer_edit', requirements: ['id' => '\d+'])]
     public function edit(
         Offers $offer,
